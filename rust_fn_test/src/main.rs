1c42ce033e4c16bd7d07c2aa8679579d61eac5d6
Joshua Megnauth <joshuamegnauth54@gmail.com>
2020-07-21 10:11:52 -0400

    Playing around with rand.

--- rust_fn_test/src/main.rs
+++ rust_fn_test/src/main.rs
@@ -1,6 +1,9 @@
-use rand::thread_rng;
+use rand::{thread_rng, Rng};
 use rand_distr::{Distribution, Normal};
 use std::vec::Vec;
+
+const NORM_MEAN: f64 = 50.0;
+const NORM_STD: f64 = 25.0;
 
 fn abs(number: f64) -> f64 {
     if number >= 0.0 {
@@ -38,11 +41,22 @@ fn factorial(n: u64) -> u64 {
 }
 
 fn main() {
-    let rng = thread_rng();
-    println!("The absolute value of -32 is {}.", abs(-32.0));
+    let mut rng = thread_rng();
 
-    let numbers = vec![14.0, 28.0, 42.0];
+    let abs_test = rng.gen_range(-25.0, 25.0);
+    println!(
+        "The absolute value of {:.1} is {:.1}.",
+        abs_test,
+        abs(abs_test)
+    );
+
+    let norm = Normal::new(NORM_MEAN, NORM_STD).unwrap();
+    let numbers = norm.sample_iter(&mut rng).take(1000).collect();
     println!("Mean: {}", mean(&numbers));
-    println!("Variance: {} Std. Dev.: {}", var(&numbers), std(&numbers));
+    println!(
+        "Variance: {:.1} Std. Dev.: {:.1}",
+        var(&numbers),
+        std(&numbers)
+    );
     println!("10!: {}", factorial(10));
 }
