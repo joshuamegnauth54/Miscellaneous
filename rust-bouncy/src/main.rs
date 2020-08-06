3a576a78ce637a3b90bff495577679fb8638a91f
Joshua Megnauth <joshuamegnauth54@gmail.com>
2020-08-06 10:53:50 -0400

    Implemented quitting and refactored the code to use mostly pancurses functions.

--- rust-bouncy/src/main.rs
+++ rust-bouncy/src/main.rs
@@ -1,5 +1,4 @@
-use pancurses::Window;
-use std::fmt::{Display, Formatter};
+use pancurses::{endwin, initscr, noecho, Input, Window};
 
 enum VertDir {
     Up,
@@ -12,15 +11,14 @@ enum HorizDir {
 }
 
 struct Ball {
-    x: u32,
-    y: u32,
+    x: i32,
+    y: i32,
     vert_dir: VertDir,
     horiz_dir: HorizDir,
 }
 
 struct Frame {
-    width: u32,
-    height: u32,
+    window: Window,
 }
 
 struct Game {
@@ -29,9 +27,9 @@ struct Game {
 }
 
 impl Game {
-    fn new(frame: Frame) -> Game {
+    fn new(sleep_duration: i32) -> Game {
         Game {
-            frame,
+            frame: Frame::new(sleep_duration),
             ball: Ball {
                 x: 2,
                 y: 4,
@@ -45,19 +43,29 @@ impl Game {
         self.ball.bounce(&self.frame);
         self.ball.mv();
     }
+
+    fn game_loop(&mut self) {
+        while self.frame.handle_quit_input() {
+            self.frame.reset();
+            self.frame.draw_border();
+            self.frame.draw_ball(&mut self.ball);
+            self.frame.update();
+            self.step();
+        }
+    }
 }
 
 impl Ball {
     fn bounce(&mut self, frame: &Frame) {
         if self.x == 0 {
             self.horiz_dir = HorizDir::Right;
-        } else if self.x == frame.width - 1 {
+        } else if self.x == frame.width() {
             self.horiz_dir = HorizDir::Left;
         }
 
         if self.y == 0 {
             self.vert_dir = VertDir::Down;
-        } else if self.y == frame.height - 1 {
+        } else if self.y == frame.height() {
             self.vert_dir = VertDir::Up;
         }
     }
@@ -74,78 +82,57 @@ impl Ball {
     }
 }
 
-impl Display for Game {
-    fn fmt(&self, fmt: &mut Formatter) -> std::fmt::Result {
-        let top_bottom = |fmt: &mut Formatter| {
-            write!(fmt, "+")?;
-            for _ in 0..self.frame.width {
-                write!(fmt, "-")?;
-            }
-            write!(fmt, "+\n")
-        };
+impl Frame {
+    fn new(sleep_duration: i32) -> Frame {
+        let window = initscr();
+        window.timeout(sleep_duration);
+        Frame { window }
+    }
 
-        top_bottom(fmt)?;
+    fn height(&self) -> i32 {
+        self.window.get_max_y() - 1
+    }
 
-        for row in 0..self.frame.height {
-            write!(fmt, "|")?;
+    fn width(&self) -> i32 {
+        self.window.get_max_x() - 1
+    }
 
-            for column in 0..self.frame.width {
-                let c = if row == self.ball.y && column == self.ball.x {
-                    'o'
-                } else {
-                    ' '
-                };
-                write!(fmt, "{}", c)?;
-            }
+    fn reset(&self) {
+        self.window.clear();
+    }
 
-            write!(fmt, "|\n")?;
+    fn draw_border(&self) {
+        self.window.border('|', '|', '-', '-', '+', '+', '+', '+');
+    }
+
+    fn draw_ball(&self, ball: &Ball) {
+        self.window.mv(ball.y as i32, ball.x as i32);
+        self.window.addch('o');
+    }
+
+    fn update(&self) {
+        self.window.refresh();
+    }
+
+    fn handle_quit_input(&self) -> bool {
+        // Returns false on q.
+        match self.window.getch() {
+            Some(Input::Character(c)) => match c {
+                'q' => false,
+                _ => true,
+            },
+            _ => true,
         }
-
-        top_bottom(fmt)
     }
 }
 
-impl Game {
-    fn draw_border(&self, window: &pancurses::Window) {
-        let vlen = self.frame.height as i32 - 1;
-        let hlen = self.frame.width as i32 - 1;
-
-        window.mv(0, 1);
-        window.addch('+');
-        window.hline('-', hlen);
-        window.mv(0, 1);
-        window.vline('|', vlen);
-        window.mv(0, hlen);
-        window.addch('+');
-        window.vline('|', vlen);
-        window.mv(vlen, 1);
-        window.addch('+');
-        window.hline('-', hlen);
-        window.mv(vlen, hlen);
-        window.addch('+');
-    }
-
-    fn draw_ball(&self, window: &pancurses::Window) {
-        window.mv(self.ball.y as i32, self.ball.x as i32);
-        window.addch('o');
+impl Drop for Frame {
+    fn drop(&mut self) {
+        endwin();
     }
 }
 
 fn main() {
-    let window = pancurses::initscr();
-    let (max_y, max_x) = window.get_max_yx();
-    let mut game = Game::new(Frame {
-        height: max_y as u32 - 1,
-        width: max_x as u32 - 1,
-    });
-
-    let sleep_duration = std::time::Duration::from_millis(33);
-    loop {
-        window.clear();
-        game.draw_border(&window);
-        game.draw_ball(&window);
-        window.refresh();
-        game.step();
-        std::thread::sleep(sleep_duration);
-    }
+    let mut game = Game::new(33);
+    game.game_loop();
 }
